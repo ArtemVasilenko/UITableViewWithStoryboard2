715042d72db3fb0b5ef5b06af2c816f52896d85c
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-22 21:14:50 +0300

    start

--- UITableViewWithStoryboard2/ViewController.swift
+++ UITableViewWithStoryboard2/ViewController.swift
@@ -1,20 +1,68 @@
-//
-//  ViewController.swift
-//  UITableViewWithStoryboard2
-//
-//  Created by Артем on 5/22/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var edit: UIBarButtonItem!
+    var idCell = "Cell"
+    
+    var textArray: [String] = {
+        var arr: [String] = []
+        
+        for _ in 0...1000 {
+            var text: String = ""
+            let count = Int.random(in: 3...8)
+            
+            for _ in 0...count {
+                text += "\(Int.random(in: 0...9))"
+            }
+            arr.append(text)
+        }
+        
+        return arr
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func editButton(_ sender: Any) {
+        myTableView.isEditing = !myTableView.isEditing
+    }
+    
+    
+}
 
+//TODO: сделать контролируемое кол-во загрузки строк
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return textArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: idCell, for: indexPath)
+        cell.textLabel?.text = textArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        
+        
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            textArray.remove(at: indexPath.row)
+            myTableView.deleteRows(at: [indexPath], with: .top)
+            myTableView.del
+        }
+        
+    }
+    
 }
 
