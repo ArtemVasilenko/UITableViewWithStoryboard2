984a877948f2fe4cdc9c691ee6041a968fc186e2
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-22 20:40:55 +0300

    Initial Commit

---
+++ UITableViewWithStoryboard2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewWithStoryboard2
+//
+//  Created by Артем on 5/22/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
